bfd90ddab85adecf94de1a744c64c2eeaaec07b6
ElTruquini <d_olaya@hotmail.com>
2016-03-17 21:57:23 -0700

    Heap class completed

--- Heap.java
+++ Heap.java
@@ -7,12 +7,9 @@
  * Date: March 17, 2016 
 */
 
-
 import java.util.NoSuchElementException;
 
-
 @SuppressWarnings({"unchecked"})
-
 
 public class Heap {
 
@@ -21,19 +18,30 @@ public class Heap {
 
 	private Comparable[] heapArray;
 
+	//Creates an empty heap
 	public Heap() {
 		heapArray = new Comparable[MAX];// REVISIT SIZE THAT I WANT TO USE**********************
 		size = 0;
 	}
 
+	/**
+	* @return True if the heap is empty, false if it is not.
+	*/
 	public boolean isEmpty (){
 		return size==0; //heapArray.size() == 0;
 	}
 
+	/**
+	* @return The number of items in the heap.
+	*/
 	public int size(){
 		return size;
 	}
 
+	/**
+	 * Inserts an item into the heap.
+	 * @param item - The newly added item.
+	 */
 	public void insert (java.lang.Comparable item){
 		heapArray[size]= item;
 
@@ -52,69 +60,93 @@ public class Heap {
 		size ++;
 	}
 
+	/**
+	* Removes the item at the root node of the heap.
+	* @return The item at the root of the heap.
+	* @throws java.util.NoSuchElementException - if the heap is empty.
+	*/
 	public java.lang.Comparable removeRootItem(){
-		if (heapArray[0] ==null){
+		if (size == 0){
 			throw new RuntimeException("Heap is empty.");
 		}
 		//copy the item from the last node into the root
 		heapArray[0]=heapArray[size-1];
 		size --;
 		heapRebuild(0, size);
-		return heapArray[0]; // NEED TO CHANGE THIS ***************
+		return heapArray[0]; 
 	}
 
+	/**
+	* Retrieves, without removing the item in the root.
+	* @return The top item in the tree.
+	* @throws java.lang.NullPointerException - if the heap is empty.
+	*/
+	public java.lang.Comparable getRootItem(){
+		if (size == 0){
+			throw new RuntimeException ("Heap is empty");
+		}
+		return heapArray[0];
+	}
+
+	//Internal helper method used to rebuild heap after deletion of root.
 	public void heapRebuild (int root, int size){
+		Comparable[] temp = new Comparable[1];
 		System.out.println("\nHEAP REBUILD");
 		int child, rightChild; 
 		if (root+1 <= size ){ //if root is not leaf
 			child = 2 * root +1;
 			if (root + 2 <= size ){ //root has right child
 				rightChild = child +1;
-				//System.out.println("Child: " + heapArray[child]);
-				//System.out.println("Rightchild " + heapArray[rightChild]);
+				//System.out.println("PRE-Child: " + heapArray[child]+ " root num " +root);
+				//System.out.println("PRE-Rightchild " + heapArray[rightChild]+ " child num " + child);
 				if (heapArray[rightChild].compareTo(heapArray[child]) < 0){
 					child = rightChild;
-					//System.out.println("I made it in the IF");
 				}
 			}
 			//root is smaller than child
-			System.out.println("Root " + heapArray[root]);
-			System.out.println("Child " + heapArray[child]);
-			if (heapArray[root].compareTo(heapArray[child]) > 0) {
-				System.out.println("Root is smaller than child");
+			//System.out.println("Root " + heapArray[root] + " root num " +root);
+			//System.out.println("Child " + heapArray[child] + " child num " + child);
+			if ((heapArray[child] != null)&&(heapArray[root].compareTo(heapArray[child]) > 0)  ) {
+				temp[0] = heapArray[root];
+				heapArray[root] = heapArray[child];
+				heapArray[child] = temp[0];
 
-
-				//AQUI ME QUEDE, PAGINA 671 DEL LIBRO. TENGO QUE TERMINAR
-				//SWAP ITEMS RECURSIVE.
-				//FUNCIONO HASTA LINEA 84
+				print();
+				heapRebuild(child, size);
 			}
 		}	
 	}
 
-
+	// Internal helper method, prints the heap array
 	public void print (){
-		System.out.println("\n PRINTING");
+		//System.out.println("\n PRINTING");
 		for (int i = 0; i < size ; i++ ){
 			System.out.println(heapArray[i]);
 		}
 	}
 
+	// Used for internal testing purposes
 	public static void main(String[] args) {
+		/*
 		Heap test = new Heap ();
 		ER_Patient p1 = new ER_Patient("Walk-in"); //4
 		ER_Patient p2 = new ER_Patient("Life-threatening"); //1
 		ER_Patient p3 = new ER_Patient("Chronic"); //2
 		ER_Patient p4 = new ER_Patient("Major fracture"); //3
 		ER_Patient p5 = new ER_Patient("Chronic"); //2
+		ER_Patient p6 = new ER_Patient("Walk-in"); //4
 
 		//System.out.println(p1);
 		//System.out.println(p5);
-
+		test.removeRootItem();
+		test.getRootItem();
+		
 		test.insert(p1);
 		test.insert(p2);
 		test.insert(p3);
 		test.insert(p4);
-		
+		test.insert(p5);
+		test.insert(p6);
 
 		test.print();
 		
@@ -123,6 +155,8 @@ public class Heap {
 		//System.out.println("Compare:");
 		//System.out.println(p1.compareTo(p2));
 		//test.heapRebuild(0, test.size());
+		*/	
+
 	}
 
 }
